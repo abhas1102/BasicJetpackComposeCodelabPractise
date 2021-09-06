ad54f50d32ae80869858c3321b262a57efc6fbe4
Abhas Kumar <abhas1102@gmail.com>
2021-09-06 13:37:24 +0530

    Used state in compose to change the color of box on clicking. Worked on External and Internal state both.

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.composecodelab
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -13,6 +15,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -34,12 +40,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecodelab.ui.theme.ComposeCodelabTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val fontFamily = FontFamily(
+      /*  val fontFamily = FontFamily(
             Font(R.font.roboto_black, FontWeight.Black),
             Font(R.font.roboto_bold, FontWeight.Bold),
             Font(R.font.roboto_italic, FontWeight.ExtraBold),
@@ -47,9 +54,24 @@ class MainActivity : ComponentActivity() {
             Font(R.font.roboto_medium, FontWeight.Medium),
             Font(R.font.roboto_regular, FontWeight.Normal),
             Font(R.font.roboto_thin, FontWeight.Thin)
-        )
+        ) */
         setContent {
-            Box(
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ){
+                    color.value = it
+                }
+                
+                Box(modifier = Modifier.background(color.value).weight(1f).fillMaxSize())
+            }
+  
+           /* Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(Color(0xFF101010))
@@ -86,11 +108,32 @@ class MainActivity : ComponentActivity() {
                     textAlign = TextAlign.Center,
                     textDecoration = TextDecoration.Underline
                 )
-            }
+            } */
 
 
         }
     }
+}
+
+
+@Composable
+fun ColorBox(modifier:Modifier = Modifier,updateColor: (Color) -> Unit){
+  // val color = remember {mutableStateOf(Color.Yellow)}
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+
+        }
+    )
 }
 
 
